3cd5bd6d2119df9096d534ba6d64dd1e127a10f6
K. Akatsuki <thevortexspirit@gmail.com>
2021-02-24 17:39:11 -0500

    refactor: move types to util

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './types';
+export * from './util/types';
 
 export * from './Map';
 export * from './Set';
--- src/util/types.ts
+++ src/util/types.ts
@@ -1,7 +1,9 @@
-import { ExtendedMap } from './Map';
-import { ExtendedWeakMap } from './WeakMap';
+import { ExtendedMap } from '../Map';
+import { ExtendedWeakMap } from '../WeakMap';
 
 type MapLike<K, V> = K extends object ? ExtendedWeakMap<K, V> : ExtendedMap<K, V>;
+
+export type IterableEntries<K, V> = Iterable<readonly [K, V]> | ReadonlyArray<readonly [K, V]>;
 
 export interface EmplaceHandler<K, V> {
 	insert?(key?: K, map?: MapLike<K, V>): V;
